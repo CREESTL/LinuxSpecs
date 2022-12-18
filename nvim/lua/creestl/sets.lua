86bdbd06bbaf0e207af6d014ebbe41dc05e7acf6
CREESTL <kroymw3@yandex.ru>
2022-12-18 18:07:24 +0300

    Do Sets in a Cycle

--- nvim/lua/creestl/sets.lua
+++ nvim/lua/creestl/sets.lua
@@ -1,43 +1,50 @@
--- Show relative line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
+local options = {
+  backup = false,                          -- creates a backup file
+  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 0,                        -- so that `` is visible in markdown files
+  fileencoding = "utf-8",                  -- the encoding written to a file
+  hlsearch = true,                         -- highlight all matches on previous search pattern
+  incsearch = true,                        -- search incrementally rather then the whole word
+  ignorecase = true,                       -- ignore case in search patterns
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  pumheight = 10,                          -- pop up menu height
+  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  showtabline = 0,                         -- do not shwo tabs (2 to show)
+  smartcase = true,                        -- smart case
+  smartindent = true,                      -- make indenting smarter again
+  splitbelow = true,                       -- force all horizontal splits to go below current window
+  splitright = true,                       -- force all vertical splits to go to the right of current window
+  swapfile = false,                        -- creates a swapfile
+  termguicolors = true,                    -- set term gui colors (most terminals support this)
+  timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true,                         -- enable persistent undo
+  updatetime = 300,                        -- faster completion (4000ms default)
+  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+  tabstop = 4,                             -- insert 4 spaces for a tab
+  cursorline = true,                       -- highlight the current line
+  number = true,                           -- set numbered lines
+  relativenumber = true,                   -- set relative numbered lines
+  numberwidth = 2,                         -- set number column width to 2 {default 4}
+  signcolumn = "no",                      -- always show the sign column, otherwise it would shift the text each time
+  wrap = false,                             -- display lines as one long line
+  linebreak = false,                        -- companion to wrap, don't split words
+  scrolloff = 8,                           -- minimal number of screen lines to keep above and below the cursor
+  sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
+  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
+}
 
--- Tab width is 4 spaces
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
--- Indent autocatically
-vim.opt.smartindent = true
-
--- Disable word wrao
-vim.opt.wrap = false
-
--- Copy to clipboard
-vim.opt.clipboard = unnamedplus
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-
--- Highlight search matches
-vim.opt.hlsearch = true
--- Use incremental search
-vim.opt.incsearch = true
-
--- Use good quality colors
-vim.opt.termguicolors = true
-
--- Leave 8 lines till the top/bottom of the screen while scrolling
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
--- Use low update time
-vim.opt.updatetime = 50
-
--- Leader symbol is space
-vim.g.mapleader = " "
-
-
+-- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help 'shortmess'
+vim.opt.shortmess:append "c"                           -- don't give |ins-completion-menu| messages
+vim.opt.iskeyword:append "-"                           -- hyphenated words recognized by searches
+vim.opt.formatoptions:remove({ "c", "r", "o" })        -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")  -- separate vim plugins from neovim in case vim still in use
 
