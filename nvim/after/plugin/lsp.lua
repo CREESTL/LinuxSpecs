865ef9a0a9925abc0570ca2b1bd7cc6cf8fb4b8c
CREESTL <kroymw3@yandex.ru>
2022-12-19 21:07:37 +0300

    Add Keymap to Cancel Autocompletion

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -16,6 +16,7 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-l>'] = cmp.mapping.close(),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 -- disable completion with tab
