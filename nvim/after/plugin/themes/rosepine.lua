7ef42760d9e291c4e3575097a5e8686dab176e5d
CREESTL <kroymw3@yandex.ru>
2023-01-11 00:27:36 +0300

    Custom Colors for Rose Pine.

--- nvim/after/plugin/themes/rosepine.lua
+++ nvim/after/plugin/themes/rosepine.lua
@@ -5,7 +5,7 @@ require('rose-pine').setup({
 	dim_nc_background = false,
 	disable_background = false,
 	disable_float_background = false,
-	disable_italics = false,
+	disable_italics = true,
 
 	--- @usage string hex value or named color from rosepinetheme.com/palette
 	groups = {
@@ -21,6 +21,16 @@ require('rose-pine').setup({
 		info = 'foam',
 		warn = 'gold',
 
+        git_add = 'foam',
+		git_change = 'rose',
+		git_delete = 'love',
+		git_dirty = 'rose',
+		git_ignore = 'muted',
+		git_merge = 'iris',
+		git_rename = 'pine',
+		git_stage = 'iris',
+		git_text = 'rose',
+
 		headings = {
 			h1 = 'iris',
 			h2 = 'foam',
@@ -33,11 +43,36 @@ require('rose-pine').setup({
 		-- headings = 'subtle'
 	},
 
-	-- Change specific vim highlight groups
+    -- THESE GROUPS ARE FOR SOLIDITY SPECIFICALLY
+    -- COMMENT THEM TO USE DEFAULT COLORS FOR OTHER LAGUAGES
+
 	highlight_groups = {
-		ColorColumn = { bg = 'rose' }
+		ColorColumn = { bg = 'rose' },
+        ['@constructor'] = { fg = 'gold', style = "bold" },
+        ['@keyword'] = { fg = 'gold', style = "bold" },
+
+        ['@variable'] = { fg = 'text' },
+        ['@number'] = { fg = 'foam' },
+        ['@string'] = { fg = 'iris' },
+        ['@field'] = { fg = 'pine' },
+        ['@constant.builtin'] = { fg = 'love', style = 'italic' },
+
+        ['@operator'] = { fg = 'rose' },
+        ['@repeat'] = { fg = 'rose' },
+        ['@conditional'] = { fg = 'rose' },
+
+        ['@type'] = { fg = 'iris' },
+        ['@type.qualifier'] = { fg = 'love', style = "italic" },
+
+        ['@storageclass'] = { fg = 'iris', style = "italic" },
+
+        ['@preproc'] = { fg = 'foam' },
+
+
+        ['@punctuation.bracket'] = { fg = 'foam' },
+        ['@punctuation.delimiter'] = { fg = 'foam' },
 	}
 })
 
 -- set colorscheme after options
--- vim.cmd('colorscheme rose-pine')
+vim.cmd('colorscheme rose-pine')
