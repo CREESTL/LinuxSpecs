9d39bf5cd16c9f21d808dbf9a6f01edcdc4f49fb
CREESTL <creestl.main@yandex.ru>
2024-01-17 13:45:12 +0300

    Add more colorschemes

--- nvim/lua/creestl/plugins/colorscheme.lua
+++ nvim/lua/creestl/plugins/colorscheme.lua
@@ -6,56 +6,73 @@ return {
     "rose-pine/neovim",
     priority = 1000,
     config = function()
-        require("rose-pine").setup({
-            dark_variant = 'main',
-            bold_vert_split = false,
-            dim_nc_background = false,
-            disable_background = false,
-            disable_float_background = false,
-            disable_italics = false,
+        require('rose-pine').setup({
+            variant = "auto", -- auto, main, moon, or dawn
+            dark_variant = "main", -- main, moon, or dawn
+            dim_inactive_windows = false,
+            extend_background_behind_borders = true,
+
+            styles = {
+                bold = true,
+                italic = false,
+                transparency = false,
+            },
 
             groups = {
-                background = 'base',
-                panel = 'surface',
-                border = 'highlight_med',
-                comment = 'muted',
-                link = 'iris',
-                punctuation = 'subtle',
+                border = "muted",
+                link = "iris",
+                panel = "surface",
 
-                error = 'love',
-                hint = 'iris',
-                info = 'foam',
-                warn = 'gold',
+                error = "love",
+                hint = "iris",
+                info = "foam",
+                warn = "gold",
 
-                git_add = 'foam',
-                git_change = 'rose',
-                git_delete = 'love',
-                git_dirty = 'rose',
-                git_ignore = 'muted',
-                git_merge = 'iris',
-                git_rename = 'pine',
-                git_stage = 'iris',
-                git_text = 'rose',
+                git_add = "foam",
+                git_change = "rose",
+                git_delete = "love",
+                git_dirty = "rose",
+                git_ignore = "muted",
+                git_merge = "iris",
+                git_rename = "pine",
+                git_stage = "iris",
+                git_text = "rose",
+                git_untracked = "subtle",
 
                 headings = {
-                    h1 = 'iris',
-                    h2 = 'foam',
-                    h3 = 'rose',
-                    h4 = 'gold',
-                    h5 = 'pine',
-                    h6 = 'foam',
-                }
-                -- or set all headings at once
-                -- headings = 'subtle'
+                    h1 = "iris",
+                    h2 = "foam",
+                    h3 = "rose",
+                    h4 = "gold",
+                    h5 = "pine",
+                    h6 = "foam",
+                },
+                -- Alternatively, set all headings at once.
+                -- headings = "subtle",
             },
-
 
             highlight_groups = {
-                ColorColumn = { bg = 'rose' },
+                -- Comment = { fg = "foam" },
+                -- VertSplit = { fg = "muted", bg = "muted" },
             },
+
+            before_highlight = function(group, highlight, palette)
+                -- Disable all undercurls
+                -- if highlight.undercurl then
+                --     highlight.undercurl = false
+                -- end
+                --
+                -- Change palette colour
+                -- if highlight.fg == palette.pine then
+                --     highlight.fg = palette.foam
+                -- end
+            end,
         })
 
-        vim.cmd("colorscheme rose-pine")
+        vim.cmd('colorscheme rose-pine')
+        -- vim.cmd('colorscheme rose-pine-main')
+        -- vim.cmd('colorscheme rose-pine-moon')
+        -- vim.cmd('colorscheme rose-pine-dawn')
     end,
 }
 
