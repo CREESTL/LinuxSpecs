efd8de0863c869b9a618113aaea8becca4ebb801
CREESTL <creestl.main@yandex.ru>
2024-02-29 13:29:25 +0300

    Leave only 2 colorschemes

--- nvim/lua/creestl/plugins/colorscheme.lua
+++ nvim/lua/creestl/plugins/colorscheme.lua
@@ -1,125 +1,85 @@
--- Colorscheme
-
--- #################### ROSEPINE ####################
+-- Colorscheme 
 return {
 
     "rose-pine/neovim",
     priority = 1000,
     config = function()
-        require('rose-pine').setup({
-            variant = "auto", -- auto, main, moon, or dawn
-            dark_variant = "main", -- main, moon, or dawn
-            dim_inactive_windows = false,
-            extend_background_behind_borders = true,
-
-            styles = {
-                bold = true,
-                italic = false,
-                transparency = false,
-            },
-
+        require("rose-pine").setup({
+            dark_variant = 'main',
+            bold_vert_split = false,
+            dim_nc_background = false,
+            disable_background = false,
+            disable_float_background = false,
+            disable_italics = true,
+            
             groups = {
-                border = "muted",
-                link = "iris",
-                panel = "surface",
-
-                error = "love",
-                hint = "iris",
-                info = "foam",
-                warn = "gold",
-
-                git_add = "foam",
-                git_change = "rose",
-                git_delete = "love",
-                git_dirty = "rose",
-                git_ignore = "muted",
-                git_merge = "iris",
-                git_rename = "pine",
-                git_stage = "iris",
-                git_text = "rose",
-                git_untracked = "subtle",
-
+                background = 'base',
+                panel = 'surface',
+                border = 'highlight_med',
+                comment = 'muted',
+                link = 'iris',
+                punctuation = 'subtle',
+                
+                error = 'love',
+                hint = 'iris',
+                info = 'foam',
+                warn = 'gold',
+                
+                git_add = 'foam',
+                git_change = 'rose',
+                git_delete = 'love',
+                git_dirty = 'rose',
+                git_ignore = 'muted',
+                git_merge = 'iris',
+                git_rename = 'pine',
+                git_stage = 'iris',
+                git_text = 'rose',
+                
                 headings = {
-                    h1 = "iris",
-                    h2 = "foam",
-                    h3 = "rose",
-                    h4 = "gold",
-                    h5 = "pine",
-                    h6 = "foam",
-                },
-                -- Alternatively, set all headings at once.
-                -- headings = "subtle",
+                    h1 = 'iris',
+                    h2 = 'foam',
+                    h3 = 'rose',
+                    h4 = 'gold',
+                    h5 = 'pine',
+                    h6 = 'foam',
+                }
+                -- or set all headings at once
+                -- headings = 'subtle'
             },
-
+            
+            
             highlight_groups = {
-                -- Comment = { fg = "foam" },
-                -- VertSplit = { fg = "muted", bg = "muted" },
+                ColorColumn = { bg = 'rose' },
             },
-
-            before_highlight = function(group, highlight, palette)
-                -- Disable all undercurls
-                -- if highlight.undercurl then
-                --     highlight.undercurl = false
-                -- end
-                --
-                -- Change palette colour
-                -- if highlight.fg == palette.pine then
-                --     highlight.fg = palette.foam
-                -- end
-            end,
         })
 
-        vim.cmd('colorscheme rose-pine')
-        -- vim.cmd('colorscheme rose-pine-main')
-        -- vim.cmd('colorscheme rose-pine-moon')
-        -- vim.cmd('colorscheme rose-pine-dawn')
+        vim.cmd("colorscheme rose-pine")
     end,
+
+
+	-- "ellisonleao/gruvbox.nvim",
+	-- priority = 1000,
+	-- config = function()
+ --        require("gruvbox").setup({
+ --            undercurl = true,
+ --            underline = true,
+ --            bold = true,
+ --            italic = {
+ --                strings = false,
+ --                comments = false,
+ --            },
+ --            strikethrough = true,
+ --            invert_selection = false,
+ --            invert_signs = false,
+ --            invert_tabline = false,
+ --            invert_intend_guides = false,
+ --            inverse = true, -- invert background for search, diffs, statuslines and errors
+ --            contrast = "hard", -- can be "hard", "soft" or empty string
+ --            palette_overrides = {},
+ --            overrides = {},
+ --            dim_inactive = false,
+ --            transparent_mode = false,
+ --        })
+ --        vim.cmd("colorscheme gruvbox")
+ --    end,
 }
-
--- #################### NORD ####################
--- return {
---     'shaunsingh/nord.nvim',
---     priority = 1000,
---     config = function()
---         require("lualine").setup({
---             options = {
---                 theme = "nord"
---             }
---         })
---
---         vim.g.nord_italic = false
---         vim.g.nord_bold = true
---
---         vim.cmd("colorscheme nord")
---     end,
--- }
-
-
--- #################### GRUVBOX ####################
--- return {
--- "ellisonleao/gruvbox.nvim",
--- priority = 1000,
--- config = function()
---        require("gruvbox").setup({
---            undercurl = true,
---            underline = true,
---            bold = true,
---            italic = {
---                strings = false,
---                comments = false,
---            },
---            strikethrough = true,
---            invert_selection = false,
---            invert_signs = false,
---            invert_tabline = false,
---            invert_intend_guides = false,
---            inverse = true, -- invert background for search, diffs, statuslines and errors
---            contrast = "hard", -- can be "hard", "soft" or empty string
---            palette_overrides = {},
---            overrides = {},
---            dim_inactive = false,
---            transparent_mode = false,
---        })
---        vim.cmd("colorscheme gruvbox")
---    end,
--- }
