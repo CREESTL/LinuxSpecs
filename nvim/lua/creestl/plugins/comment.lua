39acf1d4eadd7834247b77fd3659d85a982bb711
CREESTL <creestl.main@yandex.ru>
2024-06-17 19:22:46 +0300

    Add comments for multiline.

--- nvim/lua/creestl/plugins/comment.lua
+++ nvim/lua/creestl/plugins/comment.lua
@@ -1,7 +1,7 @@
 -- Comment out multiple lines
 
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = true -- runs require('Comment').setup()
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = true -- runs require('Comment').setup()
 }
