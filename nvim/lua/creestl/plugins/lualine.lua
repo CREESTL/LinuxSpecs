3875ed205fb67e3e3b8bc89cc718abac226501cd
CREESTL <creestl.main@yandex.ru>
2024-12-15 23:08:08 +0300

    Change Lualine style and components.

--- nvim/lua/creestl/plugins/lualine.lua
+++ nvim/lua/creestl/plugins/lualine.lua
@@ -1,16 +1,14 @@
 -- Prettier line at the bottom
 
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-    
+    "nvim-lualine/lualine.nvim",
+    config = function()
         local lualine = require("lualine")
 
         lualine.setup({
             options = {
                 icons_enabled = true,
                 theme = 'auto',
-                component_separators = { left = '', right = '' },
                 section_separators = { left = '', right = '' },
                 always_divide_middle = true,
                 globalstatus = false,
@@ -24,15 +22,22 @@ return {
                 lualine_a = { 'mode' },
                 lualine_b = {
                     {
-                        'branch',
-                        'diff',
-                        colored = true,
-                        diff_color = {
-                            added    = 'DiffAdd',
-                            modified = 'DiffChange',
-                            removed  = 'DiffDelete',
+                        'filename',
+                        file_status = true,
+                        newfile_status = true,
+                        path = 0,
+                        shorting_target = 30,
+                        symbols = {
+                            modified = '[+]',
+                            readonly = '[RO!]',
+                            unnamed = '[No Name]',
+                            newfile = '[New]',
                         },
-                        symbols = { added = '+', modified = '~', removed = '-' },
+                        -- Separators do not work for filename
+                    }
+                },
+                lualine_c = {
+                    {
                         'diagnostics',
                         sources = { 'nvim_lsp' },
                         sections = { 'error', 'warn' },
@@ -41,27 +46,27 @@ return {
                             warn  = 'DiagnosticWarn',
                         },
                         symbols = { error = 'E', warn = 'W' },
+                        always_visible = false,
+                        update_in_insert = false,
+                        separator = { left='┃'},
                     }
                 },
-                lualine_c = {
+                lualine_x = {
                     {
-                        'filename',
-                        file_status = false,
-                        newfile_status = false,
-                        path = 3,
-                        shorting_target = 40,
-                        symbols = {
-                            modified = '[+]',
-                            readonly = '[-]',
-                            unnamed = '[Unnamed]',
-                            newfile = '[New]',
-                        }
+                        'branch',
+                        colored = true,
+                        diff_color = {
+                            added    = 'DiffAdd',
+                            modified = 'DiffChange',
+                            removed  = 'DiffDelete',
+                        },
+                        symbols = { added = '+', modified = '~', removed = '-' },
                     }
                 },
-                lualine_x = { 'filetype' },
+                lualine_y = { 'filetype' },
                 lualine_z = { 'location' }
             },
-        })
 
+        })
     end,
 }
