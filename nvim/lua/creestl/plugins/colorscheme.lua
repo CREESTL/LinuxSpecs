b6fae3d8a0bb1fb31ee0e5bf78293379e0b2d79f
CREESTL <creestl.main@yandex.ru>
2024-12-19 13:54:58 +0300

    Add Catppuccin theme.

--- nvim/lua/creestl/plugins/colorscheme.lua
+++ nvim/lua/creestl/plugins/colorscheme.lua
@@ -38,144 +38,209 @@
 --         vim.cmd("colorscheme gruvbox")
 --     end,
 -- }
+--
 
 -- Lackluster
+-- return {
+--     "slugbyte/lackluster.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--
+--         local lackluster = require("lackluster")
+--
+--         -- Tweak colors
+--         -- This should be done in a separate 'setup()' call
+--         lackluster.setup({
+--             -- Change color values
+--             -- you can set a value to a custom hexcode like' #aaaa77' (hashtag required)
+--             -- or if the value is 'default' or nil it will use lackluster's default color
+--             tweak_color = {
+--                 lack = "default",
+--                 luster = "default",
+--                 orange = "default",
+--                 yellow = "#e1cf1c",
+--                 green = "default",
+--                 blue = "default",
+--                 red = "default",
+--                 -- WARN: Changing these is a bad idea
+--                 -- black = "default",
+--                 -- gray1 = "default",
+--                 -- gray2 = "default",
+--                 -- gray3 = "default",
+--                 -- gray4 = "default",
+--                 -- gray5 = "default",
+--                 -- gray6 = "default",
+--                 -- gray7 = "default",
+--                 -- gray8 = "default",
+--                 -- gray9 = "default",
+--             },
+--
+--         })
+--
+--         local color = lackluster.color -- blue, green, red, orange, black, lack, luster, gray1-9
+--
+--         lackluster.setup({
+--             -- Change colors of specific syntax elements.
+--             -- This is a full list of all supported elements.
+--             -- https://github.com/slugbyte/lackluster.nvim/blob/6d206a3af7dd2e8389eecebab858e7d97813fc0c/lua/lackluster/tweak.lua#L49
+--             tweak_syntax = {
+--                 -- String types
+--                 string = color.orange,
+--                 -- ???
+--                 string_escape = color.orange,
+--                 -- Comments
+--                 comment = color.gray5,
+--                 -- Built-int language functions
+--                 builtin = color.lack,
+--                 -- Types
+--                 type = color.lack,
+--                 -- All keywords except "return"
+--                 keyword = color.yellow,
+--                 -- "return"-like keyword
+--                 keyword_return = color.yellow,
+--                 -- "exception"-like keywords
+--                 keyword_exception = color.orange,
+--             },
+--             -- Change background colors.
+--             -- Use "none" for transparency.
+--             tweak_background = {
+--                 -- Background of normal editor mode
+--                 normal = color.gray2,
+--                 -- Telescope windows
+--                 telescope = 'default',
+--                 -- nvim_cmp, wildmenu ...
+--                 menu = 'default',
+--                 -- lazy, mason, whichkey ...
+--                 popup = 'default',
+--             },
+--             -- Change specific Treesitter highlight groups.
+--             -- To see highlight group of element under the cursor, use Treesitter Playground ":Inspect":
+--             -- https://github.com/nvim-treesitter/playground
+--             -- See :help nvim_set_hl for more available keys.
+--             -- overwrite == true will force overwrite lackluster's default highlights
+--             -- overwrite == false will extend/update lackluster's defaults (nil also does this)
+--             tweak_highlight = {
+--                 -- Variables
+--                 ["@variable"] = {
+--                     overwrite = true,
+--                     fg = color.luster
+--                 },
+--                 ["@variable.parameter"] = {
+--                     overwrite = true,
+--                     link = "@variable"
+--                 },
+--                 ["@variable.member"] = {
+--                     overwrite = true,
+--                     link = "@variable"
+--                 },
+--                 ["@variable.builtin"] = {
+--                     overwrite = true,
+--                     fg = color.lack
+--                 },
+--                 -- Functions
+--                 ["@function"] = {
+--                     overwrite = true,
+--                     fg = color.luster
+--                 },
+--                 ["@function.call"] = {
+--                     overwrite = true,
+--                     fg = color.gray7
+--                 },
+--                 ["@function.method.call"] = {
+--                     overwrite = true,
+--                     link = "@function.call"
+--                 },
+--                 ["@function.method"] = {
+--                     overwrite = true,
+--                     link = "@function.call"
+--                 },
+--                 ["@function.builtin"] = {
+--                     overwrite = true,
+--                     fg = color.lack
+--                 },
+--                 ["@constructor"] = {
+--                     overwrite = true,
+--                     link = "@function.call"
+--                 },
+--                 -- Properties
+--                 ["@property"] = {
+--                     overwrite = true,
+--                     fg = color.gray7
+--                 },
+--             },
+--             -- Disable plugin affects for other plugins.
+--             -- Set "true" to disable for chosen plugin.
+--             disable_plugin = {
+--                 telescope = true,
+--                 tree = true,
+--             },
+--         })
+--
+--         vim.cmd.colorscheme("lackluster-hack")
+--     end,
+-- }
+
+-- Catpuccin
 return {
-    "slugbyte/lackluster.nvim",
-    lazy = false,
-    priority = 1000,
+    "catppuccin/nvim",
+    priority=1000,
     config = function()
+        local catpuccin = require("catppuccin")
 
-        local lackluster = require("lackluster")
-
-        -- Tweak colors
-        -- This should be done in a separate 'setup()' call
-        lackluster.setup({
-            -- Change color values
-            -- you can set a value to a custom hexcode like' #aaaa77' (hashtag required)
-            -- or if the value is 'default' or nil it will use lackluster's default color
-            tweak_color = {
-                lack = "default",
-                luster = "default",
-                orange = "default",
-                yellow = "#e1cf1c",
-                green = "default",
-                blue = "default",
-                red = "default",
-                -- WARN: Changing these is a bad idea
-                -- black = "default",
-                -- gray1 = "default",
-                -- gray2 = "default",
-                -- gray3 = "default",
-                -- gray4 = "default",
-                -- gray5 = "default",
-                -- gray6 = "default",
-                -- gray7 = "default",
-                -- gray8 = "default",
-                -- gray9 = "default",
+        catpuccin.setup({
+            flavour = "macchiato", -- latte, frappe, macchiato, mocha
+            background = { -- :h background
+                light = "latte",
+                dark = "mocha",
             },
-
-        })
-
-        local color = lackluster.color -- blue, green, red, orange, black, lack, luster, gray1-9
-
-        lackluster.setup({
-            -- Change colors of specific syntax elements.
-            -- This is a full list of all supported elements.
-            -- https://github.com/slugbyte/lackluster.nvim/blob/6d206a3af7dd2e8389eecebab858e7d97813fc0c/lua/lackluster/tweak.lua#L49
-            tweak_syntax = {
-                -- String types
-                string = color.orange,
-                -- ???
-                string_escape = color.orange,
-                -- Comments
-                comment = color.gray5,
-                -- Built-int language functions
-                builtin = color.lack,
-                -- Types
-                type = color.lack,
-                -- All keywords except "return"
-                keyword = color.yellow,
-                -- "return"-like keyword
-                keyword_return = color.yellow,
-                -- "exception"-like keywords
-                keyword_exception = color.orange,
+            transparent_background = false, -- disables setting the background color.
+            show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+            term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+            dim_inactive = {
+                enabled = false, -- dims the background color of inactive window
+                shade = "dark",
+                percentage = 0.15, -- percentage of the shade to apply to the inactive window
             },
-            -- Change background colors.
-            -- Use "none" for transparency.
-            tweak_background = {
-                -- Background of normal editor mode
-                normal = color.gray2,
-                -- Telescope windows
-                telescope = 'default',
-                -- nvim_cmp, wildmenu ...
-                menu = 'default',
-                -- lazy, mason, whichkey ...
-                popup = 'default',
+            no_italic = true, -- Force no italic
+            no_bold = false, -- Force no bold
+            no_underline = false, -- Force no underline
+            styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" }, -- Change the style of comments
+                conditionals = { "italic" },
+                loops = { "bold" },
+                functions = { "bold" },
+                keywords = { "bold" },
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+                -- miscs = {}, -- Uncomment to turn off hard-coded styles
             },
-            -- Change specific Treesitter highlight groups.
-            -- To see highlight group of element under the cursor, use Treesitter Playground ":Inspect":
-            -- https://github.com/nvim-treesitter/playground
-            -- See :help nvim_set_hl for more available keys.
-            -- overwrite == true will force overwrite lackluster's default highlights
-            -- overwrite == false will extend/update lackluster's defaults (nil also does this)
-            tweak_highlight = {
-                -- Variables
-                ["@variable"] = {
-                    overwrite = true,
-                    fg = color.luster
+            color_overrides = {},
+            custom_highlights = {},
+            default_integrations = true,
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                notify = false,
+                barbar = true,
+                harpoon = true,
+                mason = true,
+                telescope=true,
+                which_key=true,
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
                 },
-                ["@variable.parameter"] = {
-                    overwrite = true,
-                    link = "@variable"
-                },
-                ["@variable.member"] = {
-                    overwrite = true,
-                    link = "@variable"
-                },
-                ["@variable.builtin"] = {
-                    overwrite = true,
-                    fg = color.lack
-                },
-                -- Functions
-                ["@function"] = {
-                    overwrite = true,
-                    fg = color.luster
-                },
-                ["@function.call"] = {
-                    overwrite = true,
-                    fg = color.gray7
-                },
-                ["@function.method.call"] = {
-                    overwrite = true,
-                    link = "@function.call"
-                },
-                ["@function.method"] = {
-                    overwrite = true,
-                    link = "@function.call"
-                },
-                ["@function.builtin"] = {
-                    overwrite = true,
-                    fg = color.lack
-                },
-                ["@constructor"] = {
-                    overwrite = true,
-                    link = "@function.call"
-                },
-                -- Properties
-                ["@property"] = {
-                    overwrite = true,
-                    fg = color.gray7
-                },
-            },
-            -- Disable plugin affects for other plugins.
-            -- Set "true" to disable for chosen plugin.
-            disable_plugin = {
-                telescope = true,
-                tree = true,
+                -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
             },
         })
-
-        vim.cmd.colorscheme("lackluster-hack")
+        vim.cmd.colorscheme("catppuccin")
     end,
 }
